42e7b927c80d1d39c43b89210387cc9042cb5ff2
Ian Zhang <yingzhang.ian@gmail.com>
2018-04-30 23:21:48 +0800

    done k10.

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -18,6 +18,18 @@ fun todoTask10(): Nothing = TODO(
 
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, todoTask10())
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(o1: Int?, o2: Int?): Int {
+            return if (o1 == null && o2 == null) {
+                0
+            } else if (o1 == null) {
+                1
+            } else if (o2 == null) {
+                -1
+            } else {
+                o2 - o1
+            }
+        }
+    })
     return arrayList
 }
